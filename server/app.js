74ef6f8995c7d0706fa0d25b109983f896c50727
Francesco Vicidomini <fr.vicidomini94@gmail.com>
2018-12-05 12:48:19 +0100

    added signleton folder and an example of a test case

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 
-var app = express();
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
