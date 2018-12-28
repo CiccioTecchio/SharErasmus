305650020d9f262ea61e5c78407b1f46614389c4
Rosaria <r.iorio11@studenti.unisa.it>
2018-12-28 20:26:24 +0100

    Create ChatGroup
    Creato il lato client della chat di gruppo

--- docs/js/chat.js
+++ docs/js/chat.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
   
-  var arr= [];
+  var arr= [];//List users
+  var count= 0; //Numero gruppi
+
+  //Gruppo
+  $(".creaGruppo").hide();
+  $(document).on('click','#groupChat', function(){
+    $(".creaGruppo").show();
+  });
+  $(document).on('click','#annullaGruppo', function(){
+    $(".creaGruppo").hide();
+  });
+
 
   $(document).on('click', '.msg_head', function() { 
     var chatbox = $(this).parents().attr("rel") ;
@@ -36,8 +47,31 @@ $(document).ready(function(){
   $("body").append(  chatPopup  );
 displayChatBox();
 });
-  
 
+$(document).on('click', '#creaGruppoButtons', function() {
+  $(".creaGruppo").hide();
+  count= count + 1;
+  var groupID = $(this).attr("id") + count;
+  var username = $("input[type=text][name=nomeGruppo]").val();
+  
+  if ($.inArray(groupID, arr) != -1)
+  {
+      arr.splice($.inArray(groupID, arr), 1);
+     }
+  
+  arr.unshift(groupID);
+  chatPopup = '<div class="msg_box" style="right:240px" rel="'+ groupID + '">'+
+  '<div class="msg_head">' + username +            
+    '<div class="buttonsChat"><button id="settingsChat"><i class="fa fa-cog fa-fw" aria-hidden="true"></i> </button>'+
+          '<div class="closeChat">x</div></div> </div>'+
+  '<div class="msg_wrap"><div class="msg_body"></div>'+
+      '<div class="msg_footer"><textarea class="msg_input" ></textarea>' +
+          '<button id="inviaMsg"><i class="fa fa-send" aria-hidden="true"></i></button>'+
+          '<button id="allegaFile"><i class="fa fa-paperclip"aria-hidden="true"></i></button> </div> </div></div>';
+  $("body").append(  chatPopup  );
+displayChatBox();
+});
+  
 
 function displayChatBox(){ 
   i = 270 ; // start position
@@ -54,6 +88,8 @@ $.each( arr, function( index, value ) {
   }
      });  
 } 
+
+
 });
 function openForm() {
   document.getElementById("chatForm").style.display = "block";
