08bae31e9a08eef076cccce6721c792e2028aea8
Fonz <a.ruggiero114@studenti.unisa.it>
2019-01-09 22:11:54 +0100

    Update travis

--- server/test/travis.sql
+++ server/test/travis.sql
@@ -1,14 +1,7 @@
 CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
-/*CREATE DATABASE  IF NOT EXISTS `progetto` /*!40100 DEFAULT CHARACTER SET utf8 ;
-USE `progetto`;*/
 CREATE DATABASE  IF NOT EXISTS `progetto` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `progetto`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: progetto
--- ------------------------------------------------------
--- Server version	5.6.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -41,6 +34,7 @@ CREATE TABLE `coordinatore` (
   `facolta` varchar(100) NOT NULL,
   `imgProfiloPath` varchar(50) DEFAULT NULL,
   `passToken` varchar(20) DEFAULT NULL,
+  `rating` int(11),
   PRIMARY KEY (`emailCoordinatore`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -174,7 +168,7 @@ CREATE TABLE `studente` (
   `status` enum('Normale','Partito','Tornato') NOT NULL,
   `bio` varchar(500) DEFAULT NULL,
   `imgProfiloPath` varchar(50) DEFAULT NULL,
-  `passToken` varchar(20) DEFAULT NULL
+  `passToken` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`emailStudente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -291,4 +285,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-30 19:55:58
+-- Dump completed on 2019-01-07 17:33:02
