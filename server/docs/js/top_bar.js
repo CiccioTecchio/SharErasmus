13c7cdda3ea67ea5c85c1afd826d82c0b4c67ae6
Francesco Breve <f.breve@studenti.unisa.it>
2019-01-16 15:21:55 +0100

    Top Bar implemented
    Implementata la top bar e effettuati diverse modifche e bugfix a PRU
    Ignoriate le cartelle /upload e /docs_timeline

--- server/docs/js/top_bar.js
+++ server/docs/js/top_bar.js
@@ -1,31 +1,49 @@
-var title = document.title;
+$(document).ready(function () {
+    let title = document.title;
+    let emailTop = localStorage.getItem("email");
 
-var emailTop = localStorage.getItem("email");
+    let split = emailTop.split("@");
+    if (split[1] == "unisa.it") {
+
+    } else {
+        document.getElementById("mappa").hidden = true;
+        document.getElementById("list").hidden = true;
+    }
+
+    switch (title) {
+        case "Home":
+            document.getElementById("home").className = "active";
+            break;
+        case "Forum":
+            document.getElementById("forum").className = "active";
+            break;
+        case "Mappa":
+            document.getElementById("mappa").className = "active";
+            break;
+        case "F.A.Q.":
+            document.getElementById("faq").className = "active";
+            break;
+        case "Lista Studenti":
+            document.getElementById("list").className = "active";
+            break;
+    }
 
 
-var split = emailTop.split("@");
-if(split[1] == "unisa.it"){
-    
-}else{
-   document.getElementById("mappa").hidden = true;
-   document.getElementById("list").hidden = true;
+
+    $.get('user/visualizzaDA?email=' + emailTop, function(data){
+        let nome = data.nome;
+        let cognome = data.cognome;
+        $("#userTopBar").text(nome + " " + cognome);
+        console.log(nome + " " + cognome);
+    })
+})
+
+function goToProfile(){
+    location.href="./profil_users.html"
 }
 
-switch(title){
-    case "Home":
-    document.getElementById("home").className = "active";
-    break;
-    case "Forum":
-    document.getElementById("forum").className = "active";
-    break;
-    case "Mappa":
-    document.getElementById("mappa").className = "active";
-    break;
-    case "F.A.Q.":
-    document.getElementById("faq").className = "active";
-    break;
-    case "Lista Studenti":
-    document.getElementById("list").className = "active";
-    break;
+function logout(){
+    let email = localStorage.getItem("email");
+    localStorage.setItem("email", "");
+    window.location.href = "/index.html";
 }
-
